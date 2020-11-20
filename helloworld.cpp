58aa66749caa6ab76cecf0f86ffaf030be6a45ce
Ubuntu <ubuntu@ip-172-31-42-232.ec2.internal>
2020-11-20 18:08:59 +0000

    commit

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+//Copyright(C) 2020 Tong Gong All Right received
+//
+// Created by Tong Gong
+// Created time November 2020
+// This is a "hello world" program with propr rstyle 
+
+#include <iostream>
+
+int main(){
+	std::cout << "Hello World!" << std::endl;
+}
